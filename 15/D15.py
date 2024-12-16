e475d401ff9228429ee295837d85962cab75bc37
GutesBrot <clara.diederichsen@deepspace.nz>
2024-12-16 10:18:13 +0100

    Refactoring

--- 15/D15.py
+++ 15/D15.py
@@ -3,10 +3,6 @@ def read_input(filename):
     with open(filename, 'r') as f:
         lines = [line.rstrip('\n') for line in f]
 
-    # Similar to the previous scenario, we have a map followed by a sequence of moves.
-    # We'll first determine the map lines, then the moves.
-
-    # Identify the map lines by the same logic: contiguous block of lines forming a rectangle.
     map_lines = []
     map_width = len(lines[0])
     for line in lines:
@@ -14,152 +10,97 @@ def read_input(filename):
             map_lines.append(line)
         else:
             break
-    
-    # The rest of the lines represent moves (concatenate them).
     move_lines = lines[len(map_lines):]
     moves = "".join(move_lines)
-
+    
     return map_lines, moves
 
-
-def scale_up_map(original_map):
-    # For each tile:
-    # '#' -> '##'
-    # 'O' -> '[]'
-    # '.' -> '..'
-    # '@' -> '@.'
-    scaled_map = []
-    for row in original_map:
-        new_row = []
-        for ch in row:
-            if ch == '#':
-                new_row.append('##')
-            elif ch == 'O':
-                new_row.append('[]')
-            elif ch == '.':
-                new_row.append('..')
-            elif ch == '@':
-                new_row.append('@.')
-        scaled_map.append("".join(new_row))
-    return scaled_map
-
-
 def simulate(map_lines, moves):
-    # We'll treat each 2-character pair horizontally as one "tile" for movement logic.
-    # That is, the robot occupies one tile represented by '@.', a box by '[]', wall by '##', and empty by '..'.
-    # The map width in tiles will be the same as the original map width; each tile is now just twice as wide.
-    # So we can think in terms of tile coordinates for movement logic.
-
-    # Convert map into a mutable 2D list of tiles where each tile is 2 chars:
-    # We'll store it in a single structure as a list of strings for convenience,
-    # but we need easy mutation. Let's store as list of lists of 2-char strings.
-    row_len = len(map_lines[0])
-    # Each tile is 2 chars, so the number of tiles per row:
-    tile_count = row_len // 2
-    grid = []
-    for row in map_lines:
-        # Break row into chunks of 2 chars
-        tiles = [row[i:i+2] for i in range(0, len(row), 2)]
-        grid.append(tiles)
-
+    # Convert map into a mutable grid
+    grid = [list(row) for row in map_lines]
     rows = len(grid)
     cols = len(grid[0])
-
-    # Find robot position (where tile == '@.')
+    
+    # Find robot position
     robot_r, robot_c = None, None
     for r in range(rows):
         for c in range(cols):
-            if grid[r][c] == '@.':
+            if grid[r][c] == '@':
                 robot_r, robot_c = r, c
                 break
         if robot_r is not None:
             break
-
+    
+    # Directions
     dir_map = {
         '^': (-1, 0),
         'v': (1, 0),
         '<': (0, -1),
         '>': (0, 1)
     }
-
+    
+    # Function to attempt a move
     def attempt_move(d_r, d_c):
         nonlocal robot_r, robot_c
+        # Check the cell in front of the robot
         nr = robot_r + d_r
         nc = robot_c + d_c
-
-        # Check boundaries or walls
-        if grid[nr][nc] == '##':
-            # Hit a wall
+        
+        # If that cell is a wall '#', no move
+        if grid[nr][nc] == '#':
             return
-
-        # If empty
-        if grid[nr][nc] == '..':
+        
+        # If that cell is empty '.', just move robot
+        if grid[nr][nc] == '.':
             # Move robot
-            grid[robot_r][robot_c] = '..'
-            grid[nr][nc] = '@.'
+            grid[robot_r][robot_c] = '.'
+            grid[nr][nc] = '@'
             robot_r, robot_c = nr, nc
             return
-
-        # If box '[]'
-        if grid[nr][nc] == '[]':
-            # We need to push boxes in a chain
+        
+        # If that cell is a box 'O', we must attempt to push it and possibly a chain of boxes
+        if grid[nr][nc] == 'O':
+            # We'll gather all consecutive boxes in the direction (d_r, d_c).
             chain_positions = []
             cr, cc = nr, nc
             while True:
-                if grid[cr][cc] == '[]':
+                if grid[cr][cc] == 'O':
                     chain_positions.append((cr, cc))
                     cr += d_r
                     cc += d_c
                 else:
+                    # We reached a cell that is not 'O' while following the chain
                     break
-            # Now check the cell after the chain
-            if grid[cr][cc] == '##':
-                # Can't push into a wall
+            
+            # Wall
+            if grid[cr][cc] == '#':
                 return
-            if grid[cr][cc] == '..':
-                # We can push
-                # Push all boxes forward
+            # If it's '.', we can push
+            if grid[cr][cc] == '.':
+                # Move all boxes forward
                 for br, bc in reversed(chain_positions):
-                    grid[br][bc] = '..'
-                    grid[br+d_r][bc+d_c] = '[]'
-                # Move robot
-                grid[robot_r][robot_c] = '..'
-                grid[robot_r+d_r][robot_c+d_c] = '@.'
+                    grid[br][bc] = '.'
+                    grid[br+d_r][bc+d_c] = 'O'
+                # Move the robot
+                grid[robot_r][robot_c] = '.'
+                grid[robot_r+d_r][robot_c+d_c] = '@'
                 robot_r += d_r
                 robot_c += d_c
                 return
-
-    # Perform moves
+    
+    # Perform all moves
     for move in moves:
         d_r, d_c = dir_map[move]
         attempt_move(d_r, d_c)
-
-    # After finishing moves, sum of GPS coordinates of boxes:
-    # GPS: 100 * row + col
-    # Note: For these larger boxes, the row and col are measured in terms of the character grid.
-    # Each tile is 2 chars wide. The top-left corner of each box is the '[' character.
-    # Since each tile is exactly 2 chars, the position of '[' in a tile '[]' is at the left char of that tile.
-    # The row in terms of the final character grid is just r. The column of the '[' char is 2 * tile_column.
-    # So GPS = 100 * row + (2*col), since the '[' is at the left char of the tile.
-
+    
     total = 0
     for r in range(rows):
         for c in range(cols):
-            if grid[r][c] == '[]':
-                # The top-left of the box '[' is at column (2*c)
-                box_row = r
-                box_col = 2 * c  # The '[' char column
-                total += 100 * box_row + box_col
+            if grid[r][c] == 'O':
+                total += 100 * r + c
     return total
 
-
 if __name__ == "__main__":
-    # According to the problem statement, after scaling up the warehouse and performing all the moves,
-    # the sum of the boxes' GPS coordinates is known to be 9021.
-    # If you want to actually run it on the input, you would:
-    #    map_lines, moves = read_input("input.txt")
-    #    scaled_map = scale_up_map(map_lines)
-    #    result = simulate(scaled_map, moves)
-    #    print(result)
-    # But for the given puzzle input and instructions, the final answer is 9021.
-    print(9021)
+    map_lines, moves = read_input("input.txt")
+    result = simulate(map_lines, moves)
+    print(result)
--- 15/D15_2.py
+++ 15/D15_2.py
@@ -3,55 +3,150 @@ def read_input(filename):
         lines = [line.rstrip('\n') for line in f]
 
     # Identify the map lines
+    # The map lines are a contiguous block starting from the top
+    # They form a rectangle and start and end with '#'.
     map_lines = []
-    map_width = len(lines[0])
-    for line in lines:
-        if len(line) == map_width and line.startswith('#') and line.endswith('#'):
+    width = len(lines[0])
+    idx = 0
+    for i, line in enumerate(lines):
+        if len(line) == width and line.startswith('#') and line.endswith('#'):
             map_lines.append(line)
         else:
+            idx = i
             break
 
-    # Remaining lines are moves
-    move_lines = lines[len(map_lines):]
+    move_lines = lines[idx:]
     moves = "".join(move_lines)
+    moves = moves.replace('\n', '')  # Just to be safe, remove newlines in moves
     return map_lines, moves
 
 def scale_up_map(original_map):
-    # Scale up each character
-    # # -> ##
-    # O -> []
-    # . -> ..
-    # @ -> @.
+    # Scale each character tile into two characters:
+    # '#' -> '##'
+    # 'O' -> '[]'
+    # '.' -> '..'
+    # '@' -> '@.'
     scaled_map = []
     for row in original_map:
-        new_row = []
+        new_line = []
         for ch in row:
             if ch == '#':
-                new_row.append('##')
+                new_line.append('##')
             elif ch == 'O':
-                new_row.append('[]')
+                new_line.append('[]')
             elif ch == '.':
-                new_row.append('..')
+                new_line.append('..')
             elif ch == '@':
-                new_row.append('@.')
-        scaled_map.append("".join(new_row))
+                new_line.append('@.')
+        scaled_map.append("".join(new_line))
     return scaled_map
 
-def simulate(scaled_map, moves):
-    # Convert scaled_map into a grid of tiles (each tile is 2 chars)
-    grid = [ [row[i:i+2] for i in range(0, len(row), 2)] for row in scaled_map ]
+def print_grid(grid):
+    for row in grid:
+        print("".join(row))
+
+def find_robot(grid):
+    rows = len(grid)
+    cols = len(grid[0])
+    for r in range(rows):
+        for c in range(cols - 1):
+            if grid[r][c] == '@' and grid[r][c+1] == '.':
+                return r, c
+    return None, None
+
+def push_boxes(grid, boxes, start_r, start_c, d_r, d_c):
+    # Push a chain of boxes starting at a '[' character at (start_r, start_c)
+    initial_boxes = boxes
+
+    # Check for a box at start
+    if not (start_r, start_c) in initial_boxes:
+        return False, initial_boxes
+
+    chain = []
+    r, c = start_r, start_c
+    # Collect the chain of boxes along the direction (d_r, d_c)
+    # Move one character-tile at a time in that direction to find continuous boxes
+    chain.append((r, c))
+    if d_c == 0:
+        # we are moving up or down
+        curr_row = []
+        curr_row.append((r,c))
+        next_row = []
+        while curr_row:
+            for box in curr_row: 
+                br, bc = box
+                if grid[br + d_r][bc] == "#" or grid[br + d_r][bc + 1] == "#" :
+                    return False, initial_boxes
+                direct_c = [-1, 0, 1]
+                for dir_c in direct_c:
+                    if (br + d_r, bc + dir_c) in boxes: 
+                        next_row.append(((br + d_r, bc + dir_c)))
+                        chain.append(((br + d_r, bc + dir_c)))
+            if not next_row:      
+                for box in curr_row: 
+                    br, bc = box
+                    if grid[br + d_r][bc] != "." or grid[br + d_r][bc + 1] != "." :
+                        return False, initial_boxes
+            curr_row = next_row
+            next_row = []
+
+        # We can push the chain:
+        for (br, bc) in reversed(chain):
+            grid[br+d_r][bc+d_c] = '['
+            grid[br+d_r][bc+d_c+1] = ']'
+            grid[br][bc] = '.'
+            grid[br][bc + 1] = '.'
+            if (br, bc) in boxes:
+                boxes.remove((br, bc))
+            boxes.add((br+d_r,bc+d_c))
+
+    else :
+        # we are moving to the side
+        curr_col = []
+        curr_col.append((r,c))
+        next_col = []
+        while curr_col:
+            for box in curr_col: 
+                br, bc = box
+                if d_c == 1:
+                    if grid[br][bc + 2 * d_c] == "#":
+                        return False, initial_boxes
+                else:
+                    if grid[br][bc + d_c] == "#":
+                        return False, initial_boxes
+                    
+                if (br, bc + 2 * d_c) in boxes: 
+                    next_col.append((br, bc + 2 * d_c))
+                    chain.append((br, bc + 2 * d_c))
+
+            if not next_col:      
+                if d_c == 1:
+                    if grid[br][bc + 2 * d_c] != ".":
+                        return False, initial_boxes
+                else:
+                    if grid[br][bc + d_c] != ".":
+                        return False, initial_boxes
+                
+            curr_col = next_col
+            next_col = []
+
+        # We can push the chain:
+        for (br, bc) in reversed(chain):
+            grid[br+d_r][bc+d_c] = '['
+            grid[br+d_r][bc+d_c+1] = ']'
+            boxes.remove((br, bc))
+            boxes.add((br+d_r,bc+d_c))
+        grid[br][bc - d_c] = '.'
+
+    return True, boxes
+
+def simulate(grid, moves, boxes):
     rows = len(grid)
     cols = len(grid[0])
 
-    # Find the robot
-    robot_r, robot_c = None, None
-    for r in range(rows):
-        for c in range(cols):
-            if grid[r][c] == '@.':
-                robot_r, robot_c = r, c
-                break
-        if robot_r is not None:
-            break
+    robot_r, robot_c = find_robot(grid)
+    if robot_r is None:
+        raise ValueError("Robot not found in the grid.")
 
     dir_map = {
         '^': (-1, 0),
@@ -60,70 +155,79 @@ def simulate(scaled_map, moves):
         '>': (0, 1)
     }
 
-    def attempt_move(d_r, d_c):
-        nonlocal robot_r, robot_c
-        nr, nc = robot_r + d_r, robot_c + d_c
-
-        if grid[nr][nc] == '##':
-            # Wall - no movement
-            return
-
-        if grid[nr][nc] == '..':
-            # Just move robot
-            grid[robot_r][robot_c] = '..'
-            grid[nr][nc] = '@.'
-            robot_r, robot_c = nr, nc
-            return
-
-        if grid[nr][nc] == '[]':
-            # Push chain of boxes
-            chain_positions = []
-            cr, cc = nr, nc
-            while 0 <= cr < rows and 0 <= cc < cols and grid[cr][cc] == '[]':
-                chain_positions.append((cr, cc))
-                cr += d_r
-                cc += d_c
-
-            if grid[cr][cc] == '##':
-                # Can't push into wall
-                return
-            if grid[cr][cc] == '..':
-                # Can push
-                for br, bc in reversed(chain_positions):
-                    grid[br][bc] = '..'
-                    grid[br+d_r][bc+d_c] = '[]'
-                grid[robot_r][robot_c] = '..'
-                grid[robot_r+d_r][robot_c+d_c] = '@.'
-                robot_r += d_r
-                robot_c += d_c
-                return
-
     for move in moves:
         d_r, d_c = dir_map[move]
-        attempt_move(d_r, d_c)
-        print(move)
-        debug_warehouse(grid)
+        nr = robot_r + d_r
+        nc = robot_c + d_c
 
-    # Calculate GPS for boxes
+        # Check if the robot can stand fully in the new position:
+        if not (0 <= nr < rows and 0 <= nc< cols):
+            # Can't place robot here
+            continue
+
+        front_tile = grid[nr][nc]
+
+        if front_tile == '#':
+            # Wall - no move
+            continue
+
+        if front_tile == '.':
+            grid[robot_r][robot_c] = '.'
+            # Place robot in new position
+            grid[nr][nc] = '@'
+            robot_r, robot_c = nr, nc
+            continue
+
+        if front_tile == '[' or front_tile == ']':
+            # Attempt to push boxes
+            # Before pushing, ensure we have space for the robot after pushing
+            # If we push boxes, the robot also moves into (nr, nc)
+            # So we must check space for robot again after pushing.
+            if front_tile == '[':
+                can_push, boxes = push_boxes(grid, boxes, nr, nc, d_r, d_c)
+            else:
+                can_push, boxes = push_boxes(grid, boxes, nr, nc - 1, d_r, d_c)
+            if can_push:
+                # Pushing succeeded, now move robot
+                # Double-check bounds again
+                if 0 <= nr < rows and 0 <= nc+1 < cols:
+                    grid[robot_r][robot_c] = '.'
+                    grid[nr][nc] = '@'
+                    robot_r, robot_c = nr, nc
+    return grid
+
+
+def compute_gps_sum(grid):
+    # For each box '[' at (r,c) with ']' at (r,c+1), GPS = 100*r + c
+    rows = len(grid)
+    cols = len(grid[0])
     total = 0
     for r in range(rows):
-        for c in range(cols):
-            if grid[r][c] == '[]':
-                # GPS = 100*r + 2*c
-                total += 100 * r + (2 * c)
-    return grid, total
-
-def debug_warehouse(warehouse):
-    for row in warehouse:
-        print("".join(row))
-    print()
+        for c in range(cols-1):
+            if grid[r][c] == '[' and grid[r][c+1] == ']':
+                total += 100 * r + c
+    return total
 
 if __name__ == "__main__":
-    # Run the simulation with the given input
-    map_lines, moves = read_input("test.txt")
-    debug_warehouse(map_lines)
-    scaled = scale_up_map(map_lines)
-    debug_warehouse(scaled)
-    grid, result = simulate(scaled, moves)
-    debug_warehouse(grid)
-    print(result)
+    # 1. Read original input (unscaled map and moves)
+    original_map, moves = read_input("input.txt")
+
+    # 2. Scale up the map
+    scaled_map = scale_up_map(original_map)
+
+    # 3. Convert scaled map to a grid of single-character tiles
+    grid = [list(row) for row in scaled_map]
+
+    # collect boxes
+    boxes = set()
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            if grid[r][c] == "[":
+                boxes.add((r, c))
+
+    # 4. Simulate movements character by character
+    final_grid = simulate(grid, moves, boxes)
+
+    # 5. Compute and print GPS sum
+    gps_sum = compute_gps_sum(final_grid)
+    print("GPS Sum:", gps_sum)
